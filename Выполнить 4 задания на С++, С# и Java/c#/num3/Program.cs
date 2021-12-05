cafd41fd05dde4b455c9346445c22466cdad3ec6
elya.tkd.itf <elya.tkd.itf@gmail.com>
2021-12-06 01:22:26 +0300

    do c# num3

---
+++ Выполнить 4 задания на С++, С# и Java/c#/num3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace num3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("input.txt"))
+                {
+                    HashSet<char> chars = new HashSet<char>();
+                    chars.Add('e'); chars.Add('y'); chars.Add('u'); 
+                    chars.Add('o'); chars.Add('a'); chars.Add('i');
+                    String[] strings = sr.ReadLine().Split(' ');
+                    foreach (var str in strings)
+                    {
+                        if (chars.Contains(str.ElementAt(str.Length - 1)))
+                            Console.WriteLine(str);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
