c5e88a5b85d8c2e8476ecb9034a36496d888c9fd
elya.tkd.itf <elya.tkd.itf@gmail.com>
2021-12-05 22:28:20 +0300

    do c++ num2

---
+++ Выполнить 4 задания на С++, С# и Java/c++/num1/num2/num2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Book.cpp"
+
+using namespace std;
+
+int main()
+{
+    Book book("ll", "kl", 1234);
+    book.diskOut();
+    Book book2;
+    book2.diskIn();
+    cout << book2.toString() << endl;;
+    return 0;
+}
