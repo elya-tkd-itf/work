f1679ac3ff46547a28b4154b228d12b1e5bb0b24
elya.tkd.itf <elya.tkd.itf@gmail.com>
2021-12-06 00:48:43 +0300

    do c# num1

---
+++ Выполнить 4 задания на С++, С# и Java/c#/num1/Book.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace num1
+{
+    public class Book
+    {
+        private String name, author;
+        private int year;
+        public Book()
+        {
+            this.name = "No name";
+            this.author = "No author";
+            this.year = -1;
+        }
+        public Book(String name, String author, int year)
+        {
+            this.name = name;
+            this.author = author;
+            this.year = year;
+        }
+
+        public int yearsFrom(int nowYear)
+        {
+            return nowYear - year;
+        }
+
+        private bool leap(int year)
+        {
+            return (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+        }
+
+        public int daysFrom(int nowYear, int nowMonth, int nowDay)
+        {
+            int[] daysInMonth = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int days = 0;
+            for (int year = this.year; year < nowYear; year++)
+            {
+                if (leap(year)) days += 366;
+                else days += 365;
+            }
+            for (int month = 0; month < nowMonth; month++)
+            {
+                if (month == 1 && leap(nowYear)) days += 1;
+                days += daysInMonth[month];
+            }
+            return days;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public String getAuthor()
+        {
+            return author;
+        }
+
+        public void setAuthor(String author)
+        {
+            this.author = author;
+        }
+
+        public int getYear()
+        {
+            return year;
+        }
+
+        public void setYear(int year)
+        {
+            this.year = year;
+        }
+    }
+
+}
---
+++ Выполнить 4 задания на С++, С# и Java/c#/num1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace num1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Lol", "Mark Shtolts", 1980);
+            Console.WriteLine(book.yearsFrom(2021));
+            Console.WriteLine(book.daysFrom(2021, 12, 5));
+            Console.ReadKey();
+        }
+    }
+}
