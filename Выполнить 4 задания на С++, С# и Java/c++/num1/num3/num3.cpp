61a2ad6815fc631266ce5eec330df7172d21869c
elya.tkd.itf <elya.tkd.itf@gmail.com>
2021-12-05 22:48:11 +0300

    Auto stash before merge of "main" and "origin/main"

---
+++ Выполнить 4 задания на С++, С# и Java/c++/num1/num3/num3.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <fstream>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+    ifstream in("input.txt");
+    set<char> chars{ 'e', 'y', 'u', 'i', 'o', 'a' };
+    if (in.is_open()) {
+        string str;
+        while (in >> str) {
+            if (chars.find(str.at(str.size() - 1)) != chars.end()) 
+                cout << str << " ";
+        }
+    }
+}
