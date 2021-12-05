a460de0193da4e99c1e0aa382e27dd73e21ce574
elya.tkd.itf <elya.tkd.itf@gmail.com>
2021-12-06 01:09:50 +0300

    do c# num2

---
+++ Выполнить 4 задания на С++, С# и Java/c#/num2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace num2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Lol", "Mark_Shtolts", 1980);
+            book.diskOut();
+            Book book2 = new Book();
+            book2.diskIn();
+            Console.WriteLine(book2.ToString());
+            Console.WriteLine(book2.yearsFrom(2021));
+            Console.WriteLine(book2.daysFrom(2021, 12, 5));
+            Console.ReadKey();
+        }
+    }
+}
